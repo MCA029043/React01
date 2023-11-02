6e52c5a51d7f3681c4776288382b0ab4517aa881
MCA029043 <ajaypotlia99@gmail.com>
2023-11-02 22:08:59 +0530

    adding files

---
+++ src/Component/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='footer bg-gray-600 text-center p-8' >
+      <p>Copyright 2015. All right reserved</p>
+    </div>
+  )
+}
+
+export default Footer
